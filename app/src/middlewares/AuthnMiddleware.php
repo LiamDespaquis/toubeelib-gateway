a6278ffa01a0cee0ed15326ed844c72e83a3a61b
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-01-08 15:21:56 +0100

    rm auth

--- app/src/middlewares/AuthnMiddleware.php
+++ app/src/middlewares/AuthnMiddleware.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace toubeelib\middlewares;
 
 use DI\Container;
@@ -13,55 +14,55 @@ use Slim\Routing\RouteContext;
 use toubeelib\providers\auth\AuthInvalidException;
 use toubeelib\providers\auth\AuthnProviderInterface;
 
-class AuthnMiddleware implements MiddlewareInterface{
+class AuthnMiddleware implements MiddlewareInterface
+{
+    protected AuthnProviderInterface $authProvider;
+    protected Logger $loger;
 
-	protected AuthnProviderInterface $authProvider;
-	protected Logger $loger;
-	
-	public function __construct(Container $co)
-	{
-		$this->authProvider = $co->get(AuthnProviderInterface::class);
-		$this->loger = $co->get(Logger::class)->withName("AutnhMiddleware");
-	}
+    public function __construct(Container $co)
+    {
+        $this->authProvider = $co->get(AuthnProviderInterface::class);
+        $this->loger = $co->get(Logger::class)->withName("AutnhMiddleware");
+    }
 
-	public function process(ServerRequestInterface $rq, RequestHandlerInterface $next): ResponseInterface
-	{
-		if(!$rq->hasHeader("Authorization")){
-		foreach($rq->getHeaders() as $s){
-			$this->loger->error($s[0]);
-			}
-
-			throw new HttpUnauthorizedException($rq, "Header Authorization manquante, veuillez vous enregistrer");
-		}
-		try{
-			$token = $rq->getHeader("Authorization")[0];
-			$token = sscanf($token, "Bearer %s");
-			if($token == null){
-				throw new Exception("Mauvais token");
-			}
-			$token = $token[0];
-			$user = $this->authProvider->getSignedInUser($token);
-			$rq = $rq->withAttribute('user', $user);
-		}catch (AuthInvalidException $e){
-			$this->loger->error($e->getMessage());
-			throw new HttpUnauthorizedException($rq, "Votre authentification n'est pas valide, veuillez vous reconnecter");
-		}
-		catch(\Error $e){
-			$this->loger->error($e->getMessage());
-			throw new HttpUnauthorizedException($rq, "Erreur lors de l'authentification veuillez verifier votre token");
-		}
-
-
-		// avant requête
-		//authz middleware
-		// $authz = new AuthzMiddleware($user->role);	
-		// $rs = $authz->process($rq, $next);
+    public function process(ServerRequestInterface $rq, RequestHandlerInterface $next): ResponseInterface
+    {
+        /*if(!$rq->hasHeader("Authorization")){*/
+        /*foreach($rq->getHeaders() as $s){*/
+        /*	$this->loger->error($s[0]);*/
+        /*	}*/
+        /**/
+        /*	throw new HttpUnauthorizedException($rq, "Header Authorization manquante, veuillez vous enregistrer");*/
+        /*}*/
+        /*try{*/
+        /*	$token = $rq->getHeader("Authorization")[0];*/
+        /*	$token = sscanf($token, "Bearer %s");*/
+        /*	if($token == null){*/
+        /*		throw new Exception("Mauvais token");*/
+        /*	}*/
+        /*	$token = $token[0];*/
+        /*	$user = $this->authProvider->getSignedInUser($token);*/
+        /*	$rq = $rq->withAttribute('user', $user);*/
+        /*}catch (AuthInvalidException $e){*/
+        /*	$this->loger->error($e->getMessage());*/
+        /*	throw new HttpUnauthorizedException($rq, "Votre authentification n'est pas valide, veuillez vous reconnecter");*/
+        /*}*/
+        /*catch(\Error $e){*/
+        /*	$this->loger->error($e->getMessage());*/
+        /*	throw new HttpUnauthorizedException($rq, "Erreur lors de l'authentification veuillez verifier votre token");*/
+        /*}*/
 
 
+        // avant requête
+        //authz middleware
+        // $authz = new AuthzMiddleware($user->role);
+        // $rs = $authz->process($rq, $next);
 
-		$rs = $next->handle($rq);
-		//après requete
-		return $rs;
-	}
+
+
+        $rs = $next->handle($rq);
+        //après requete
+        return $rs;
+    }
 
 }
