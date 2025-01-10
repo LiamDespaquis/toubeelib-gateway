46bc3deb77420c42eef5804532a2d96b6884527b
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-01-10 14:46:05 +0100

    bruno

--- gateway/src/application/actions/GetPraticienById.php
+++ gateway/src/application/actions/GetPraticienById.php
@@ -2,15 +2,34 @@
 
 namespace toubeelibgateway\application\actions;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpUnauthorizedException;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpInternalServerErrorException;
 
 class GetPraticienById extends AbstractAction
 {
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
-        return $responseToubeelib = $this->client->request('GET', 'praticiens/'.$args["id"], [
-        ]);
-        return $rs;
+
+        try {
+            return $responseToubeelib = $this->client->request('GET', 'praticiens/'.$args["id"], [
+            ]);
+        } catch (ConnectException | ServerException $e) {
+            throw new HttpInternalServerErrorException($rq, " … ");
+        } catch (ClientException $e) {
+            match($e->getCode()) {
+                400 => throw new HttpBadRequestException($rq, " … "),
+                401 => throw new HttpUnauthorizedException($rq, " … "),
+                403 => throw new HttpForbiddenException($rq, " … "),
+                404 => throw new HttpNotFoundException($rq, " … ")
+            };
+        }
     }
 }
