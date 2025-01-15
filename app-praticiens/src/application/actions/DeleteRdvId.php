853a00a1c8cdf59346cfd3df5d3530996af34813
LiamDespaquis <liamdespaqiis@gmail.com>
2025-01-15 15:16:27 +0100

    debut_duplication_praticien

---
+++ app-praticiens/src/application/actions/DeleteRdvId.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace toubeelib\application\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
+use toubeelib\application\actions\AbstractAction;
+use toubeelib\application\renderer\JsonRenderer;
+use toubeelib\core\services\ServiceOperationInvalideException;
+use toubeelib\core\services\rdv\ServiceRDVInvalidDataException;
+
+class DeleteRdvId extends AbstractAction
+{
+    //annule rdv, pas supprimer
+
+    public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
+    {
+
+        $status = 200;
+        try {
+            $rdv = $this->serviceRdv->annulerRendezVous($args['id']);
+            $rs = JsonRenderer::render($rs,201, GetRdvId::ajouterLiensRdv($rdv, $rq));
+            $this->loger->info('DeleteRdv : '.$args['id'].' rdv supprimé');
+        } catch (ServiceRDVInvalidDataException $e) {
+            $this->loger->error('DeleteRdv : '.$args['id'].' : '.$e->getMessage());
+            throw new HttpNotFoundException($rq,$e->getMessage());
+        }catch (ServiceOperationInvalideException $e){
+            $this->loger->error('DeleteRdv : '.$args['id'].' : '.$e->getMessage());
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+        return $rs;
+    }
+}
